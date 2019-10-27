a898b99d456e52f18338eadc5237435cfa0c6f51
volovar <michaelvolovar@gmail.com>
2019-10-27 03:55:36 -0400

    [VLVR | 2019-10-27] Update Emotion, Refactor Class Component
    Updated Emotion to use @emotion/core and added the babel preset to allow
    the `css` prop.
    
    Refactored the `App` component to be a functional component instead of a
    class component and to use hooks.

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,75 @@
-import React, { Component } from "react"
-import AddItemForm from "./AddItemForm"
-import Item from "./Item"
+import React, { useEffect, useState } from "react";
+import AddItemForm from "./AddItemForm";
+import Item from "./Item";
+import { Global } from "@emotion/core";
+import { globalCss, headerCss } from "./app.styles";
+
 import {
     getSpreadSheetId,
     handleSignoutClick,
     handleAuthClick,
-    createSpreadsheet,
     addItem,
     removeItem,
     setupHeaders
-} from "../utils/spreadsheets"
-import { getUserInfo } from "../utils/user"
+} from "../utils/spreadsheets";
+import { getUserInfo } from "../utils/user";
 
-const SCOPES = "https://www.googleapis.com/auth/spreadsheets"
+const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
 const DISCOVERY_DOCS = [
     "https://sheets.googleapis.com/$discovery/rest?version=v4"
-]
-const SPREADSHEET_ID = getSpreadSheetId()
+];
+const SPREADSHEET_ID = getSpreadSheetId();
 
-class App extends Component {
-    state = {
-        currentUser: {
-            isSignedIn: false
-        },
-        header: [],
-        data: []
+const App = () => {
+    const [currentUser, setCurrentUser] = useState({ isSignedIn: false });
+    const [header, setHeader] = useState([]);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        window.gapi.load("client:auth2", initClient);
+    }, []);
+
+    function setOrderData(isSignedIn) {
+        if (isSignedIn) {
+            window.gapi.client.sheets.spreadsheets.values
+                .get({
+                    spreadsheetId: SPREADSHEET_ID,
+                    range: "Sheet1"
+                })
+                .then(
+                    response => {
+                        setupHeaders(response);
+                        setData(response.result.values.slice(1));
+                        setHeader(response.result.values[0]);
+                    },
+                    err => {
+                        console.log(err);
+                        // TODO: Create a new sheet if one doesn't exist
+                        // createSpreadsheet()
+                    }
+                );
+        } else {
+            setData([]);
+            setHeader([]);
+        }
     }
 
-    componentDidMount() {
-        window.gapi.load("client:auth2", this.initClient)
+    /**
+     *  Called when the signed in status changes, to update the UI
+     *  appropriately. After a sign-in, the API is called.
+     */
+    function updateSigninStatus(isSignedIn) {
+        const currentUser = getUserInfo(isSignedIn);
+
+        setCurrentUser(currentUser);
+        setOrderData(isSignedIn);
     }
 
     /**
      *  Initializes the API client library and sets up sign-in state
      *  listeners.
      */
-    initClient = () => {
+    function initClient() {
         window.gapi.client
             .init({
                 apiKey: process.env.API_KEY,
@@ -45,76 +79,33 @@ class App extends Component {
             })
             .then(
                 () => {
-                    gapi.auth2
+                    window.gapi.auth2
                         .getAuthInstance()
-                        .isSignedIn.listen(this.updateSigninStatus)
+                        .isSignedIn.listen(updateSigninStatus);
 
                     this.updateSigninStatus(
-                        gapi.auth2.getAuthInstance().isSignedIn.get()
-                    )
+                        window.gapi.auth2.getAuthInstance().isSignedIn.get()
+                    );
                 },
                 error => {
-                    console.log(JSON.stringify(error, null, 2))
+                    console.log(JSON.stringify(error, null, 2));
                 }
-            )
+            );
     }
 
-    /**
-     *  Called when the signed in status changes, to update the UI
-     *  appropriately. After a sign-in, the API is called.
-     */
-    updateSigninStatus = isSignedIn => {
-        const currentUser = getUserInfo(isSignedIn)
-
-        this.setState({ currentUser })
-        this.setOrderData(isSignedIn)
-    }
-
-    setOrderData = isSignedIn => {
-        if (isSignedIn) {
-            gapi.client.sheets.spreadsheets.values
-                .get({
-                    spreadsheetId: SPREADSHEET_ID,
-                    range: "Sheet1"
-                })
-                .then(
-                    response => {
-                        setupHeaders(response)
-
-                        this.setState({
-                            header: response.result.values[0],
-                            data: response.result.values.slice(1)
-                        })
-                    },
-                    err => {
-                        console.log(err)
-                        // TODO: Create a new sheet if one doesn't exist
-                        // createSpreadsheet()
-                    }
-                )
-        } else {
-            this.setState({
-                header: [],
-                data: []
-            })
-        }
-    }
-
-    handleAddItem = newItem => {
+    const handleAddItem = newItem => {
         addItem(newItem).then(newData => {
-            gapi.client.sheets.spreadsheets.values
+            window.gapi.client.sheets.spreadsheets.values
                 .get({
                     spreadsheetId: SPREADSHEET_ID,
                     range: newData.updates.updatedRange
                 })
                 .then(response => {
-                    console.log(response)
-                    this.setState({
-                        data: [...this.state.data, ...response.result.values]
-                    })
-                })
-        })
-    }
+                    console.log(response);
+                    setData([...data, ...response.result.values]);
+                });
+        });
+    };
 
     // async handleAddItem(newItem) {
     //     const newData = await addItem(newItem)
@@ -132,18 +123,19 @@ class App extends Component {
     //         })
     // }
 
-    handleRemoveItem = index => {
+    const handleRemoveItem = index => {
         removeItem(index).then(result => {
-            console.log("item removed")
-            console.log(result)
-        })
-    }
+            console.log("item removed");
+            console.log(result);
+        });
+    };
 
-    render() {
-        const { data, currentUser } = this.state
-        return (
+    return (
+        <>
+            <Global styles={globalCss} />
             <div>
-                <header>
+                <header css={headerCss}>
+                    <h2>Shppd</h2>
                     {currentUser.isSignedIn ? (
                         <>
                             Hello {currentUser.name}!
@@ -161,14 +153,14 @@ class App extends Component {
                             key={item[0]}
                             item={item}
                             index={i}
-                            removeItem={this.handleRemoveItem}
+                            removeItem={handleRemoveItem}
                         />
                     ))}
                 </div>
-                <AddItemForm addItem={this.handleAddItem} />
+                <AddItemForm addItem={handleAddItem} />
             </div>
-        )
-    }
-}
+        </>
+    );
+};
 
-export default App
+export default App;
